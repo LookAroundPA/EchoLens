8c1178dcb206d403ba6effd30cd2307265eb1724
LookAroundPA <90828222+LookAroundPA@users.noreply.github.com>
2026-07-13 10:46:00 +0800

    feat: expose Faster-Whisper transcription services

--- src/echolens/speech/__init__.py
+++ src/echolens/speech/__init__.py
@@ -1,1 +1,13 @@
-"""Speech recognition module."""
+"""Speech recognition services."""
+
+from echolens.speech.faster_whisper import (
+    FasterWhisperTranscriber,
+    TranscriptSegment,
+    TranscriptionResult,
+)
+
+__all__ = [
+    "FasterWhisperTranscriber",
+    "TranscriptSegment",
+    "TranscriptionResult",
+]
